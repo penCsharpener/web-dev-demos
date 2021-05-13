3c5390a55d5470dd8c8184fcb8ba76a4c8746a0d
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2021-05-14 00:29:00 +0200

    use webpack bundle instead of app.js

--- gettingStarted/js/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-function startGame() {
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
-}
-(_a = document.getElementById('startGame')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', startGame);
-//# sourceMappingURL=app.js.map
