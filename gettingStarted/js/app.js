698810b7c08c63abb9564964d60ae04299ff092c
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2021-05-14 00:19:25 +0200

    setup getting started project with webpack server

---
+++ gettingStarted/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var _a;
+function startGame() {
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+(_a = document.getElementById('startGame')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
